6d814dff1d72017035398ddcae593d56a86a95ee
vivek-mistry <vickymistry1818@gmail.com>
2025-11-10 19:22:43 +0530

    - implemented packqes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import $ from 'jquery';
+window.$ = window.jQuery = window.jquery = $;
+
+// import 'bootstrap';
+
+import 'datatables.net-bs4'; 
+import DataTable from 'datatables.net-bs5';
+
+
+// Other extensions (they automatically hook into jQuery once DataTable is bound)
+import 'datatables.net-responsive-bs5';
+
+import select2 from 'select2';
+select2(window, $);
+import 'select2/dist/js/select2.full.js';
+import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css';
+
+window.DataTable = DataTable;
+
+import 'jquery-validation';
